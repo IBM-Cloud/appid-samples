9dc04a07a4bbe695fbc157355ff764db1d711da6
nitzann <nitzann@il.ibm.com>
2018-03-29 17:38:34 +0300

    Add target and policy creation script

--- cloning-instance-with-rest/create-target-and-policy.sh
+++ cloning-instance-with-rest/create-target-and-policy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-TARGET_INSTANCE = $1
-REGION = $2
-region = $REGION
+TARGET_INSTANCE=$1
+REGION=$2
+
+SERVICE_ID_NAME=$TARGET_INSTANCE-configurator
+region=$REGION
 
 if [ $REGION == "us-south" ]; then
     region = "ng"
@@ -12,7 +14,10 @@ bx api api.$region.bluemix.net
 bx login
 bx resource service-instance-create $TARGET_INSTANCE appid graduated-tier $REGION
 
-bx iam service-id-create appid-c -d "cloning test"
-bx iam service-api-key-create appidc-key appid-c -d "appidc test key"
-bx iam service-policy-create appid-c -r Reader --service-name appid --service-instance c8295e26-bd7a-4b58-ae85-d3775691a00e
-bx iam service-policy-create appid-c -r Writer --service-name appid --service-instance 81b59d43-be0d-439c-971f-7b76f3c4f4e7
+bx iam service-id-create $SERVICE_ID_NAME -d "configurator task for $TARGET_INSTANCE"
+bx iam service-api-key-create appidc-key $SERVICE_ID_NAME
+bx iam service-policy-create $SERVICE_ID_NAME -r Reader --service-name appid
+bx iam service-policy-create $SERVICE_ID_NAME -r Writer --service-name appid --service-instance $TARGET_INSTANCE
+
+echo "App ID instance created:"
+bx resource service-instance $TARGET_INSTANCE
